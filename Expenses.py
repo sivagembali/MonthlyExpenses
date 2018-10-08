4a3d1181b95a6a14a92e81e913f108e051a8faee
cvagembali <ssiva356@gmail.com>
2018-10-08 12:45:35 +0530

    initial commit

---
+++ Expenses.py
@@ -0,0 +1,10 @@
+from flask import Flask,request,render_template,url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def showIndex():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=5000)
