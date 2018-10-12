ed4c798c9bbafdbf87a868b64ea420ebe00e35fe
sivagembali <gembalisivaprasadarao@gmail.com>
2018-10-13 01:21:40 +0530

    updated track modifications with true

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = "cdkddkdkdkdkdhdhdhd"
 db = SQLAlchemy(app)
 
